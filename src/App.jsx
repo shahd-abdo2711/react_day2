0eabb12f542acc9a3372eea56d6d57c2d13b74e0
shahd-abdo2711 <shahdhussinabdo@gmail.com>
2025-08-02 18:20:25 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
  
-import './App.css'
- 
 import Header from './components/Header'
 import Card from './components/Card'
 import data from './data'
+import './App.css'
 
 export default function App() {
     const cards = data.map(item => {
